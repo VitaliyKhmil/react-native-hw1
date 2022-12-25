b54fa389394d635ea9990bf95c67704dc095dbd7
VitaliyKhmil <hmilvitaliy@gmail.com>
2022-12-25 12:06:50 +0200

    color

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
+    backgroundColor: '#CCFFE5',
     alignItems: 'center',
     justifyContent: 'center',
   },
